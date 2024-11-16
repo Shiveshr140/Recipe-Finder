f4d1415fbd94aa4bf1f6feb6d61ba6b69e6decce
shivsehr140 <shivsehr140@gmail.com>
2024-11-16 14:14:45 +0530

    intial commit

---
+++ javascript/header.js
@@ -0,0 +1,29 @@
+function createHeaderContent() {
+  const header = document.querySelector(".header");
+  header.innerHTML = `
+      <div class="header-info">
+          <p>
+            Find Your Perfect Recipe | Explore thousands of delicious recipes curated just for you. 
+           <a href="#">Discover more</a>
+         </p>
+          <button class="header-info-button">&times;</button>
+      </div>
+      <nav>
+        <div class="nav-logo-cat-input">
+          <h1><a class="logo-link ingreedy" href="index.html"> Ingreedy </a></h1>
+          <div class="dropdown">
+              <select>
+                  <option value="" selected disabled>Categories</option>
+                  <option value="popular">Popular</option>
+                  <option value="cheap">Cheap</option>
+                  <option value="vegetarian">Vegetarian</option>
+                  <option value="gluten free">Gluten free</option>
+              </select>
+          </div>
+          <input class="search" placeholder="Start searching...." />
+        </div>
+      </nav>
+      `;
+}
+
+createHeaderContent();
