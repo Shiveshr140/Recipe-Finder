f761e732dc9ee50e6c7ad212023432ac623ecc29
Mohammad Ayan <ayankhan790521@gmail.com>
2024-11-18 23:27:11 +0530

    Two functions complete

--- javascript/helper.js
+++ javascript/helper.js
@@ -8,18 +8,63 @@ const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}
 async function popular() {
   const res = await fetch(`${url}&sort=popularity`);
   const data = await res.json();
-  console.log(data);
   return data;
 }
 
 // console.log(popular());
 
-async function cheap() {}
+async function cheap() {
+  const res = await fetch(`${url}&sort=price&sortDirection=asc`);
+  const data = await res.json();
+  return data;
+}
 
-async function vegetarian() {}
+async function vegetarian() {
+  const res = await fetch(`${url}&diet=vegetarian`);
+  const data = await res.json();
+  return data;
+}
 
-async function glutenFreen() {}
+async function glutenFreen() {
+  const res = await fetch(`${url}&intolerances=gluten`);
+  const data = await res.json();
+  return data;
+}
 
 //***************************************************************** Sidebar Recipes
 
-async function breakfastRecipes() {}
+async function breakfastRecipes() {
+  const res = await fetch(`${url}&type=breakfast`);
+  const data = await res.json();
+  return data;
+}
+
+async function lunchRecipe() {
+  const res = await fetch(`${url}&type=main course`);
+  const data = await res.json();
+  return data;
+}
+
+async function dinnerRecipe() {}
+
+async function desserts() {}
+
+async function snacks() {}
+
+async function LowCarbRecipe() {}
+
+async function ketoRecipe() {}
+
+async function italianRecipe() {}
+
+async function mexicanRecipe() {}
+
+async function chineseRecipe() {}
+
+async function indianRecipe() {}
+
+async function japaneseRecipe() {}
+
+async function mediterranianRecipe() {}
+
+async function americanRecipe() {}
