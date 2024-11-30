0806d61a2ef32110ea4b004cfaed5fb9dc0b8f0e
shivsehr140 <shivsehr140@gmail.com>
2024-11-30 20:57:24 +0530

    version 1.6

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -3,10 +3,10 @@ function createSidebarContent() {
   if (sidebar) {
     sidebar.innerHTML = `
      <div class="sidebar-recipes">
-        <a href="index.html" class="sidebar-link"> 
+        <div  class="sidebar-link-header"> 
          <img class="logo recipe-logo" src ="./style/assets/recipe-logo.svg" alt="logo" />
          <p>Recipes</p>
-        </a>
+        </div>
        </div>
        `;
   }
@@ -46,45 +46,10 @@ appendRecipeDropdown();
 module.exports = { createSidebarContent, appendRecipeDropdown };
 
 // // when the sidebar link clicked it must have some different color
-// const sidebarLink = document.querySelectorAll(".sidebar-link");
+// const sidebarLink = document.querySelectorAll(".sidebar-recipes-link");
 // sidebarLink.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     link.querySelector("p").classList.add("visited-p");
+//   link.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     link.classList.add("sidebar-visited");
 //   });
-// });
-
-//******************** Recipe link with drowpdown
-// here we are capture the event in capture phase
-
-// sidebarRecipe.addEventListener("click", (e) => {
-//   // we need to prevent the refreshing the page otherwise list will disappear and prevent the event to propagate in bublephase otherwise it will make list to disappear again
-//   e.preventDefault();
-//   e.stopPropagation();
-//   // Check if the dropdown already exists
-//   let recipeDiv = document.querySelector(".dropdown-content");
-//   if (!recipeDiv) {
-//     recipeDiv = document.createElement("div");
-//     recipeDiv.classList.add("dropdown-content");
-//     recipeDiv.innerHTML = `
-//       <ul class="sidebar-recipes-list">
-//       <li> <a href="#">Breakfast Recipes</a> </li>
-//       <li> <a href="#">Lunch Recipes</a> </li>
-//       <li> <a href="#">Dinner Recipes</a> </li>
-//       <li> <a href="#">Desserts</a> </li>
-//       <li> <a href="#">Snacks</a> </li>
-//       <li> <a href="#">Low-Carb Recipes</a> </li>
-//       <li> <a href="#">Keto Recipes</a> </li>
-//       <li> <a href="#">Italian</a> </li>
-//       <li> <a href="#">Mexican</a> </li>
-//       <li> <a href="#">Chinese</a> </li>
-//       <li> <a href="#">Indian</a> </li>
-//       <li> <a href="#">Japanese</a> </li>
-//       <li> <a href="#">Mediterranean</a> </li>
-//       <li> <a href="#">American</a> </li>
-//       </ul>
-//     `;
-//     sidebarRecipe.appendChild(recipeDiv);
-//   } else {
-//     sidebarRecipe.removeChild(recipeDiv);
-//   }
 // });
